451ebcaece41d88acbd82c4e5243224ba37458fb
Eric D Helms <ericdhelms@gmail.com>
2012-05-19 17:41:32 -0400

    Adds initial south migrations. Adds postgres setup.

--- project/settings.py
+++ project/settings.py
@@ -17,10 +17,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'covalytics',            # Or path to database file if using sqlite3.
+        'USER': 'postgres',              # Not used with sqlite3.
+        'PASSWORD': 'postgres',          # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'pipeline',
     'visuals',
     'reports',
